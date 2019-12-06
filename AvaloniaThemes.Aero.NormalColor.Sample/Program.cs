9172953a9a24a6de47a098fd0fb22744e27e1e97
Splitwirez <Splitwirez@gmail.com>
2019-12-06 16:28:12 -0500

    Add project files.

---
+++ AvaloniaThemes.Aero.NormalColor.Sample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using Avalonia.Platform;
+
+namespace AvaloniaThemes.Aero.NormalColor.Sample
+{
+    class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args) => BuildAvaloniaApp().Start<MainWindow>();
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug();
+
+        // Your application's entry point. Here you can initialize your MVVM framework, DI
+        // container, etc.
+        /*private static void AppMain(Application app, string[] args)
+        {
+            app.Run(new MainWindow());
+        }*/
+
+        private static void ConfigureAssetAssembly(AppBuilder builder)
+        {
+            AvaloniaLocator.CurrentMutable
+                .GetService<IAssetLoader>()
+                .SetDefaultAssembly(typeof(App).Assembly);
+        }
+    }
+}
