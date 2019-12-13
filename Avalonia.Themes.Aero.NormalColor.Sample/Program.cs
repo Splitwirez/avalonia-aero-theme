e539852bcecced8bda07723f13ecd447f531fc82
Splitwirez <Splitwirez@gmail.com>
2019-12-13 15:38:45 -0500

    Updated to Avalonia 0.9.0, fixed namespaces, lights `CheckBox` actually works now

--- Avalonia.Themes.Aero.NormalColor.Sample/Program.cs
+++ Avalonia.Themes.Aero.NormalColor.Sample/Program.cs
@@ -3,14 +3,14 @@ using Avalonia;
 using Avalonia.Logging.Serilog;
 using Avalonia.Platform;
 
-namespace AvaloniaThemes.Aero.NormalColor.Sample
+namespace Avalonia.Themes.Aero.NormalColor.Sample
 {
     class Program
     {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp().Start<MainWindow>();
+        public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args); //.Start<MainWindow>();
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
